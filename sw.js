a35efe4c21b5313b2e6e01d7c034b7e97986ce67
zappazanof <152647564+zappazanof@users.noreply.github.com>
2024-11-07 14:31:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var GHPATH = '/github-page-pwa';
-var APP_PREFIX = 'gppwa_';
+var GHPATH = '/Adventure';
+var APP_PREFIX = 'Adventure24';
 var VERSION = 'version_002';
 var URLS = [    
   `${GHPATH}/`,
