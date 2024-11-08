099b5721bb6ca8111939ca33052e5fd86e2a20ad
zappazanof <152647564+zappazanof@users.noreply.github.com>
2024-11-08 09:30:09 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('I am running!');
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Schneeflocken-Logik bleibt unverändert
+  
+  // Event Listener für alle Level-Buttons
+  const levelButtons = document.querySelectorAll('.levelButton');
+  
+  levelButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const level = button.getAttribute('data-level');
+      window.location.href = `level${level}.html`;
+    });
+  });
+});
