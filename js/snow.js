3d13787e5f7d1776480671e9c3f66625fb4b96d4
zappazanof <152647564+zappazanof@users.noreply.github.com>
2024-11-08 11:43:32 +0100

    Update snow.js

--- js/snow.js
+++ js/snow.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const snowflakeContainer = document.getElementById('snowflakes');
-  const snowflakeCount = 50;
+  const snowflakeCount = 100;
   
   for (let i = 0; i < snowflakeCount; i++) {
     const snowflake = document.createElement('div');
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     snowflake.textContent = '❄';
     
     snowflake.style.left = `${Math.random() * 100}vw`;
+    snowflake.style.top = `${Math.random() * 100}vh`; // Zufällige Höhe
     snowflake.style.animationDuration = `${Math.random() * 5 + 5}s`; // 5 bis 10 Sekunden
     snowflake.style.fontSize = `${Math.random() * 2 + 1}em`; // 1 bis 3 em
     snowflake.style.opacity = Math.random().toString();
