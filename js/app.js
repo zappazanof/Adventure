95ba6808b72d3683755b757babadd12c086684d5
zappazanof <152647564+zappazanof@users.noreply.github.com>
2024-11-08 11:23:06 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Event Listener für alle Level-Buttons
+  // Event Listener für alle levelButton
   const levelButtons = document.querySelectorAll('.levelButton');
   const calendar = document.getElementById('calendar');
-  
-  levelButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const level = button.getAttribute('data-level');
-      window.location.href = `level${level}.html`;
-    });
-  });
-  // Funktion zum Zufällig Anordnen der Buttons 
-  function shuffle(array) { 
+
+  // Funktion zum Zufällig Anordnen der Buttons
+  function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1)); 
+      const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
-    } return array; } 
-  // Buttons in zufälliger Reihenfolge anordnen 
-  const shuffledButtons = shuffle(Array.from(levelButtons)); 
-  calendar.innerHTML = ''; shuffled
-});
+    }
+    return array;
+  }
+
+  // Buttons in zufälliger Reihenfolge anordnen
+  const shuffledButtons = shuffle(Array.from(levelButtons));
+  calendar.innerHTML = '';
+  shuffled
