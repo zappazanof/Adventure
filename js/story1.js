6114a13eb8c53b71ea369963c32d1096f7b30d86
zappazanof <152647564+zappazanof@users.noreply.github.com>
2024-11-11 13:09:31 +0100

    Update story1.js

--- js/story1.js
+++ js/story1.js
@@ -1,9 +1,6 @@
 window.storyParts = [
   "Es war einmal in einem fernen Land, da lebte ein mutiger Ritter, der sich auf eine große Reise begab...",
-  "<img src='path/to/your/image1.jpg' alt='Ritter' class='story-image'>",
   "Auf seiner Reise traf er viele interessante Wesen und erlebte spannende Abenteuer...",
-  "<img src='path/to/your/image2.jpg' alt='Wesen' class='story-image'>",
   "Eines Tages stand er vor einem gewaltigen Drachen, dessen Augen wie Feuer glühten...",
-  "<img src='path/to/your/image3.jpg' alt='Drache' class='story-image'>",
-  // Weitere Abschnitte hinzufügen
+    // Weitere Abschnitte hinzufügen
 ];
