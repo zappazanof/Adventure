2a25978aa78bbb3a7d16006ac741cf59c465e963
zappazanof <152647564+zappazanof@users.noreply.github.com>
2024-11-12 11:10:56 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   function typeWriter(text, index, callback) {
     if (index < text.length) {
       storyContainer.innerHTML += text.charAt(index);
-      setTimeout(() => typeWriter(text, index + 1, callback), 20);
+      setTimeout(() => typeWriter(text, index + 1, callback), 5);
     } else {
       callback();
     }
